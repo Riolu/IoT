2ee363d06e39e4d99b667c11bd92986c0381749a
Riolu <hankunyan68@gmail.com>
2019-10-18 17:48:59 -0400

    stupid irc

--- src/app.py
+++ src/app.py
@@ -84,7 +84,7 @@ def getApp(dbname):
 
         # check whether the type is already in type_to_targetLoc
         type_locs = retrieve(type, "targetLocs", request.host_url, "type_to_targetLocs")
-        if type_locs is not None:
+        if targetLoc is not in type_locs:
             return {}
         
         # add to type_to_targetLoc
