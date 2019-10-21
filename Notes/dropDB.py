4bd2d8234a32311c85684396ed97b7f32916248e
Riolu <hankunyan68@gmail.com>
2019-10-21 14:46:22 -0400

    add replace

--- Notes/dropDB.py
+++ Notes/dropDB.py
@@ -1,12 +1,12 @@
-# from pymongo import MongoClient
+from pymongo import MongoClient
 import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('dbnames', nargs='+', type=str)
+    parser.add_argument('dbnames', nargs='+', type=str, help='name(s) of database(s) to drop')
     args = parser.parse_args()
-    print(args)
 
-    # client = MongoClient('localhost', 27017)
+    client = MongoClient('localhost', 27017)
 
-    # client.drop_database()
+    for dbname in args.dbnames:
+        client.drop_database(dbname)
