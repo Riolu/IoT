e598ecf1a04c804a7bf1f721e0893e69899f734d
Zefeng Liu <zefeng.liu@columbia.edu>
2019-12-03 16:49:35 -0500

    eval

--- level4/init_db.py
+++ level4/init_db.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     DB_NAME = "level4"
     URLS = [
         {"loc": "master", "url": "http://192.168.1.189:5000/"},
-        {"loc": "parent", "url": "http://192.168.1.189:5002/},
+        {"loc": "parent", "url": "http://192.168.1.189:5002/"},
         {"loc": "level5", "url": "http://192.168.1.189:5004/"},
         {"loc": "self", "url": "level4"}
     ]
