c5271a31bfe7b001d1e9eaecfd7b9e966b87a0d6
Zefeng Liu <zefeng.liu@columbia.edu>
2019-12-03 18:17:53 -0500

    eval

--- evaluation/run.sh
+++ evaluation/run.sh
@@ -13,4 +13,4 @@ elapsed="$(bc <<< "$end_time-$start_time")"
 echo "Total of $elapsed seconds elapsed for zzz"
 echo "Average time: $(bc -l <<< "$elapsed/$itrs")"
 
-curl http://192.168.1.189:5000/register -X POST -d '{"targetLoc":"level5", "td":{"_type": "pc", "id": "urn:dev:ops:54312-pc-$i"}}' -H "Content-Type:application/json"
+# curl http://192.168.1.189:5000/register -X POST -d '{"targetLoc":"level5", "td":{"_type": "pc", "id": "urn:dev:ops:54312-pc-$i"}}' -H "Content-Type:application/json"
