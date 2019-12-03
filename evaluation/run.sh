3c96ddb3c5daeb6a70c504dc332e9dfde5d72df0
Zefeng Liu <zefeng.liu@columbia.edu>
2019-12-03 18:21:25 -0500

    eval

--- evaluation/run.sh
+++ evaluation/run.sh
@@ -4,8 +4,11 @@ itrs=10
 
 start_time="$(date -u +%s.%N)"
 for ((i=1;i<=itrs;i++)); do
-     curl http://192.168.1.189:5000/register -X POST -d '{"targetLoc":"level5", "td":{"_type": "pc", "id": "\'"urn:dev:ops:54312-pc-$i"\'"}}' -H "Content-Type:application/json"
-     echo \''{"targetLoc":"level5", "td":{"_type": "pc", "id": "'"urn:dev:ops:54312-pc-$i"'"}}'\'
+    #  curl http://192.168.1.189:5000/register -X POST -d \''{"targetLoc":"level5", "td":{"_type": "pc", "id": "'"urn:dev:ops:54312-pc-$i"'"}}'\' -H "Content-Type:application/json"
+    #  echo \''{"targetLoc":"level5", "td":{"_type": "pc", "id": "'"urn:dev:ops:54312-pc-$i"'"}}'\'
+    td=\''{"_type": "pc", "id": "'"$i"'"}'\'
+    JSON=\''{"targetLoc": "level5", "td": "'"$td"'"}'\'
+    curl http://192.168.1.189:5000/register -X POST -d "$JSON" -H "Content-Type:application/json"
 done
 end_time="$(date -u +%s.%N)"
 
@@ -13,4 +16,7 @@ elapsed="$(bc <<< "$end_time-$start_time")"
 echo "Total of $elapsed seconds elapsed for zzz"
 echo "Average time: $(bc -l <<< "$elapsed/$itrs")"
 
+# curl http://192.168.1.189:5000/register -X POST -d '{"targetLoc":"level5", "td":{"_type": "pc", "id": "urn:dev:ops:54312-pc-$i"}}' -H "Content-Type:application/json"
 
+# td=\''{"_type": "pc", "id": "'"$i"'"}'\'
+# JSON=\''{"targetLoc": "level5", "td": "'"$td"'"}'\'
