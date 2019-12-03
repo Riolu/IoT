42f70a7d0073e24809aebaa6df26aca29239c18b
Zefeng Liu <zefeng.liu@columbia.edu>
2019-12-03 18:36:11 -0500

    eval

--- evaluation/run.sh
+++ evaluation/run.sh
@@ -4,9 +4,10 @@ itrs=10
 
 start_time="$(date -u +%s.%N)"
 for ((i=1;i<=itrs;i++)); do
-     JSON=\''{"targetLoc":"level5", "td":{"_type": "pc", "id": "'"urn:dev:ops:54312-pc-$i"'"}}'\'
-     echo "$JSON"
-     curl http://192.168.1.189:5000/register -X POST -d "$JSON" -H "Content-Type:application/json"
+    JSON=\''{"targetLoc":"level5", "td":{"_type": "pc", "id": "'"urn:dev:ops:54312-pc-$i"'"}}'\'
+    echo "$JSON"
+    cmd=\''curl http://192.168.1.189:5000/register -X POST -d "'"$JSON"'" -H "Content-Type:application/json"'\'
+    echo "$cmd"
     # td=\''{"_type": "pc", "id": "'"$i"'"}'\'
     # JSON=\''{"targetLoc": "level5", "td": "'"$td"'"}'\'
     # echo "$JSON"
