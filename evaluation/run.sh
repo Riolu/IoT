4f43684623e79d895aca3543bd0ec88da8042204
Zefeng Liu <zefeng.liu@columbia.edu>
2019-12-03 18:17:55 -0500

    eval

--- evaluation/run.sh
+++ evaluation/run.sh
@@ -4,7 +4,7 @@ itrs=10
 
 start_time="$(date -u +%s.%N)"
 for ((i=1;i<=itrs;i++)); do
-     curl http://192.168.1.189:5000/register -X POST -d \''{"targetLoc":"level5", "td":{"_type": "pc", "id": "'"urn:dev:ops:54312-pc-$i"'"}}'\' -H "Content-Type:application/json"
+     curl http://192.168.1.189:5000/register -X POST -d '{"targetLoc":"level5", "td":{"_type": "pc", "id": "\'"urn:dev:ops:54312-pc-$i"\'"}}' -H "Content-Type:application/json"
      echo \''{"targetLoc":"level5", "td":{"_type": "pc", "id": "'"urn:dev:ops:54312-pc-$i"'"}}'\'
 done
 end_time="$(date -u +%s.%N)"
@@ -14,3 +14,5 @@ echo "Total of $elapsed seconds elapsed for zzz"
 echo "Average time: $(bc -l <<< "$elapsed/$itrs")"
 
 # curl http://192.168.1.189:5000/register -X POST -d '{"targetLoc":"level5", "td":{"_type": "pc", "id": "urn:dev:ops:54312-pc-$i"}}' -H "Content-Type:application/json"
+
+# JSON=\''{"targetLoc": "level5", "td"}'\'
